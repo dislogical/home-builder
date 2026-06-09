b9643b9d22c7bf51cd04794933271e4de00a066b
Colden Cullen <colden@coldencullen.com>
2026-06-09 13:30:19 -0700

    feat(config): add basic content renderer

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,13 +1,49 @@
 package config
 
-import homebuilder "github.com/dislogical/home-builder/pkg"
+import (
+	"fmt"
+
+	"cuelang.org/go/pkg/encoding/json"
+	"cuelang.org/go/pkg/encoding/toml"
+	"cuelang.org/go/pkg/encoding/yaml"
+
+	homebuilder "github.com/dislogical/home-builder/pkg"
+)
 
 var _ homebuilder.ResourceStatusQueryable = (*Config)(nil)
 
-type Config struct{}
+type Config struct {
+	FileName string `json:"$filename"`
+	Encoding string `json:"$encoding"`
+	Content  string `json:"-"`
+}
 
 func Prepare(resource *homebuilder.Resource) error {
-	resource.Impl = &Config{}
+	config := &Config{}
+
+	err := resource.Config.Decode(config)
+	if err != nil {
+		return fmt.Errorf("decoding config file: %w", err)
+	}
+
+	if config.Content == "" {
+		switch config.Encoding {
+		case "json":
+			config.Content, err = json.Marshal(resource.Config)
+		case "yaml":
+			config.Content, err = yaml.Marshal(resource.Config)
+		case "toml":
+			config.Content, err = toml.Marshal(resource.Config)
+
+		default:
+			err = fmt.Errorf("unknown encoding: %s", config.Encoding)
+		}
+	}
+	if err != nil {
+		return fmt.Errorf("decoding content: %w", err)
+	}
+
+	resource.Impl = config
 
 	return nil
 }
